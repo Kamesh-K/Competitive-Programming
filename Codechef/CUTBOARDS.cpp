1c85d2a16a097f57423735fafabb049e5342cc99
Kamesh-K <kamesh3799@gmail.com>
2020-03-18 11:44:40 +0530

    Adding the template code and example

---
+++ Codechef/CUTBOARDS.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define loop(i,a,b) for(i=a;i<b;i++)
+#define tc() int t;cin>>t;while(t--)
+using namespace std;
+int main()
+{
+	int n,m;
+	tc()
+	{
+		cin>>n>>m;
+		cout<<(n-1)*(m-1)<<endl;
+	}
+}
---
+++ Template.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+double tick()
+{
+    static clock_t oldt,newt=clock();
+    double diff=1.0*(newt-oldt)/CLOCKS_PER_SEC;
+    oldt=newt;
+    return diff;
+}
+#define ll long long int
+#define ld long double
+#define loo(i,a,b) for(i=a;i<b;i++)
+#define vi vector<int>
+#define vv vector<vector<int> >
+#define ii pair<int,int>
+#define is pair<int,string>
+#define vii vector<ii>
+#define vis vector<is>
+#define pb emplace_back
+#define mp make_pair
+#define fi first
+#define se second
+#define mod 1000000007
+#define n5 100005
+#define n6 1000006
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define LINF LLONG_MAX-1001
+#define INF INT_MAX
+#define tc() int t ; cin>>t; while(t--)
+#define sl(n) scanf("%lld",&n)
+// sort(p, p+n, [=](int i, int j) { return a[i] < a[j]; }); 
+int main(){
+    fastio;
+    int n,i,j,k,x,y,z,a[n5],flag=1,count=0;
+    string s;
+    return 0;
+}
